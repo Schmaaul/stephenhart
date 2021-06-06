4511b6b1d38f6c338c34a95b57746e4d0a2c45ac
GeisterPferd <75759169+GeisterPferd@users.noreply.github.com>
2021-06-06 20:02:09 +0200

    alpabetic

--- commands/showall.js
+++ commands/showall.js
@@ -1,8 +1,17 @@
 const { getAllIds } = require("../savesystem");
 const { MessageAttachment } = require("discord.js");
 const main = (message) => {
+  const ids = getAllIds().sort(function (a, b) {
+    if (a.firstname < b.firstname) {
+      return -1;
+    }
+    if (a.firstname > b.firstname) {
+      return 1;
+    }
+    return 0;
+  });
   const attach = new MessageAttachment(
-    Buffer.from(getAllIds().join("\n")),
+    Buffer.from(ids.join("\n")),
     "itemids.txt"
   );
   message.channel.send(attach);
