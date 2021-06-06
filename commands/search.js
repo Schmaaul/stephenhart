f5c821f1c2e154c6ef07bf1302f5141192dfa591
GeisterPferd <75759169+GeisterPferd@users.noreply.github.com>
2021-06-06 20:08:03 +0200

    alphabet

--- commands/search.js
+++ commands/search.js
@@ -1,6 +1,16 @@
 const { findIdsBySearch } = require("../savesystem");
 const main = (message, command) => {
-  const ids = findIdsBySearch(command.split(" ").slice(1).join(" "));
+  const ids = findIdsBySearch(command.split(" ").slice(1).join(" ")).sort(
+    function (a, b) {
+      if (a < b) {
+        return -1;
+      }
+      if (a > b) {
+        return 1;
+      }
+      return 0;
+    }
+  );
   if (ids.length > 100)
     return message.channel.send(
       `Found ${ids.length} items specify more or use !showall`
