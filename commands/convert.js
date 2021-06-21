a2401d40467e1014243499f56f0eafcfaa5a3572
GeisterPferd <75759169+GeisterPferd@users.noreply.github.com>
2021-06-21 22:01:10 +0200

    added file support

--- commands/convert.js
+++ commands/convert.js
@@ -4,6 +4,8 @@ const { convertRoleIds, prefix } = require("../config.json");
 const strip = require("./converter/strip");
 const xml = require("./converter/xml");
 const newCon = require("./converter/new");
+
+const fetch = require("node-fetch");
 /**
  *
  * @param {import("discord.js").Message} message
@@ -20,15 +22,27 @@ const main = async (message, command) => {
     return message.channel.send(
       `Your roles dont have the permissions to use the conversion tool.`
     );
-  const attachments = message.attachments.array()[0];
-  console.log(attachments);
-  const input = strip(
-    message.content.split("").slice(`${prefix}convert`.length).join("")
-  );
-  if (xml.isRightType(input)) return xml.convert(input, message);
-  if (newCon.isRightType(input)) return newCon.convert(input, message);
-  message.channel.send(
-    "Could not identify the type of your conversion object. To convert xml your object needs to start with '<' and for the other conversion with '//'"
-  );
+  const attachment = message.attachments.array()[0];
+  //console.log(attachments);
+  if (attachment) {
+    const raw = await fetch(attachment.url)
+      .then((res) => res.text())
+      .then((body) => body);
+    const input = strip(raw);
+    if (xml.isRightType(input)) return xml.convert(input, message);
+    if (newCon.isRightType(input)) return newCon.convert(input, message);
+    message.channel.send(
+      "Could not identify the type of your conversion object. To convert xml your object needs to start with '<' and for the other conversion with '//'"
+    );
+  } else {
+    const input = strip(
+      message.content.split("").slice(`${prefix}convert`.length).join("")
+    );
+    if (xml.isRightType(input)) return xml.convert(input, message);
+    if (newCon.isRightType(input)) return newCon.convert(input, message);
+    message.channel.send(
+      "Could not identify the type of your conversion object. To convert xml your object needs to start with '<' and for the other conversion with '//'"
+    );
+  }
 };
 module.exports = main;
